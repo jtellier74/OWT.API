701500876297a12d919ce1b0aa6f1d740c009f20
Julien Tellier <julien.tellier@flowbank.com>
2022-02-06 18:48:17 +0100

    Add Skill

---
+++ OWT.Domain/Business/SkillBusiness.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using OWT.Domain.Interfaces;
+using OWT.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWT.Domain.Business
+{
+    public class SkillBusiness :ISkillBusiness
+    {
+        public ISkillManager _skillManager;
+        private readonly IMapper _mapper;
+        public SkillBusiness(ISkillManager skillManager, IMapper mapper)
+        {
+            _skillManager = skillManager;
+            _mapper = mapper;
+        }
+
+        public async Task<List<SkillModel>> GetAllSkills()
+        {
+            return await _skillManager.GetAllSkills();
+        }
+
+        public async Task<List<SkillModel>> GetAllSkillsWithContacts()
+        {
+            return await _skillManager.GetAllSkillsWithContacts();
+        }
+    }
+}
